d4fccb135fdc75d33ef88dc66b5ba7ef36a10b8a
aserafim <correia.alefe@gmail.com>
2024-05-19 15:55:47 -0300

    Adicionando funionalidades extras

--- battle/classes/game.py
+++ battle/classes/game.py
@@ -30,3 +30,45 @@ class Person:
         mgl = self.magic[i]["dmg"] - 5
         mgh = self.magic[i]["dmg"] + 5
         return random.randrange(mgl, mgh)
+    
+    def take_damage(self, dmg):
+        self.hp -= dmg
+        if self.hp < 0:
+            self.hp = 0
+        return self.hp
+    
+    def get_hp(self):
+        return self.hp
+    
+    def get_max_hp(self):
+        return self.maxhp
+    
+    def set_hp(self,hp):
+        self.hp = hp
+
+    def get_mp(self):
+        return self.mp
+    
+    def get_max_mp(self):
+        return self.maxmp
+    
+    def reduce_mp(self, cost):
+        self.mp -= cost
+
+    def get_spell_name(self, i):
+        return self.magic[i]["name"]
+    
+    def get_spell_cost(self, i):
+        return self.magic[i]["cost"]
+
+    def choose_action(self):
+        i = 1
+        for item in self.actions:
+            print(str(i) + ":" + item)
+            i += 1
+
+    def choose_magic(self):
+        i = 1
+        for spell in self.magic:
+            print(str(i) + ":" + "Spell: " + spell["name"] + " Cost: " + str(spell["cost"]))
+            i += 1
--- battle/main.py
+++ battle/main.py
@@ -6,6 +6,4 @@ magic = [{"name" : "Fire", "cost" : 10, "dmg" : 60},
 
 
 enemy = Person(460, 65, 60, 34, magic)
-print(enemy.generate_spell_damage(1))
-print(enemy.generate_spell_damage(2))
-print(enemy.generate_spell_damage(0))
+enemy.choose_magic()
