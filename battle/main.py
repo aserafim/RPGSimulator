45b4efdc8ef8f6b4df7bd6d42ded0959841740d8
aserafim <correia.alefe@gmail.com>
2024-05-19 15:26:37 -0300

    Instanciando um personagem e causando danos

---
+++ battle/main.py
@@ -0,0 +1,11 @@
+from classes.game import Person, bcolors
+
+magic = [{"name" : "Fire", "cost" : 10, "dmg" : 60},
+         {"name" : "Thunder", "cost" : 15, "dmg" : 90},
+         {"name" : "Fire", "cost" : 5, "dmg" : 30},]
+
+
+enemy = Person(460, 65, 60, 34, magic)
+print(enemy.generate_damage())
+print(enemy.generate_damage())
+print(enemy.generate_damage())
